2f23d5dac25d52395f9a54545751bfff416dddee
fredneutron <adelabu4fred@gmail.com>
2025-02-06 19:40:54 +0100

    setup Service Provider

---
+++ src/MonnifyServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Monnify\MonnifyLaravel;
+
+use Illuminate\Support\ServiceProvider;
+/**
+ * Class MonnifyServiceProvider
+ */
+class MonnifyServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/monnify.php', 'monnify');
+
+        $this->app->singleton('monnify', function($app) {
+            return new Monnify(
+                config('monnify.api_key'),
+                config('monnify.secret_key'),
+                config('monnify.environment')
+            );
+        });
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../config/monnify.php' => config_path('monnify.php'),
+        ], 'config');
+    }
+}
